4bd8cf39d0c49570b46d86a9e32b0798f7ecfb3b
JamesDKai <James.D.Kai@gmail.com>
2016-04-07 21:00:09 +0000

    Add remove list feature.

--- TodoList/assets/js/todos.js
+++ TodoList/assets/js/todos.js
@@ -18,3 +18,11 @@ $("li").click(function(){
     //     });
     // }
 });
+
+//Click on X to delete Todo
+$("span").click(function(event) {
+   $(this).parent().fadeOut(500, function(){
+       $(this).remove();
+   });
+   event.stopPropagation();
+});
