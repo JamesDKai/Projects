67096b0d61f6cc1063bad84f085b63c3f7e411fa
JamesDKai <James.D.Kai@gmail.com>
2016-04-21 01:58:32 +0000

    Add update new todo feature with the append method.

--- TodoList/assets/js/todos.js
+++ TodoList/assets/js/todos.js
@@ -1,5 +1,5 @@
 // Check Off Specific Todos By Clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
     // //if li is gray
     // if($(this).css("color") === "rgb(128, 128, 128)") {
@@ -20,9 +20,19 @@ $("li").click(function(){
 });
 
 //Click on X to delete Todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
    $(this).parent().fadeOut(500, function(){
        $(this).remove();
    });
    event.stopPropagation();
+});
+
+$("input[type='text").keypress(function(event){
+   if(event.which === 13){
+       //taking new todo text from input
+      var todoText = $(this).val();
+      $(this).val("");
+      //create a new li and add to ul
+      $("ul").append("<li><span>X</span> " + todoText + "</li>");
+   }
 });
