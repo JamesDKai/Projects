ea7b2cdad96970ca82e9e78f53cda2b8cc60fbc9
JamesDKai <James.D.Kai@gmail.com>
2017-07-01 22:39:01 +0000

    Add functional component and object restructuring to WeatherList.js

--- react-weather-app-master/src/components/WeatherList.js
+++ react-weather-app-master/src/components/WeatherList.js
@@ -1,21 +1,51 @@
 import React from 'react';
 import WeatherListItem from './WeatherListItem';
 
-class WeatherList extends React.Component {
-  render() {
-    const {days, onDayClicked} = this.props;
 
-    return <div className="weather-list flex-parent">
-      {days.map((day, index) =>
-        <WeatherListItem
-          key = {day.dt}
-          day = {day}
-          onDayClicked={onDayClicked}
-          index={index}
-          />
-      )}
-    </div>;
-  }
-}
+
+const WeatherList = ({days, onDayClicked}) =>
+//object restructing with arrow functions
+  <div className="weather-list flex-parent">
+    {days.map((day, index) =>
+      <WeatherListItem
+        key = {day.dt}
+        day = {day}
+        onDayClicked={onDayClicked}
+        index={index}
+        />
+    )}
+  </div>;
+
+// function WeatherList(props) {
+//   const {days, onDayClicked} = props;
+//
+//   return <div className="weather-list flex-parent">
+//     {days.map((day, index) =>
+//       <WeatherListItem
+//         key = {day.dt}
+//         day = {day}
+//         onDayClicked={onDayClicked}
+//         index={index}
+//         />
+//     )}
+//   </div>;
+// }
+
+// class WeatherList extends React.Component {
+//   render() {
+//     const {days, onDayClicked} = this.props;
+//
+//     return <div className="weather-list flex-parent">
+//       {days.map((day, index) =>
+//         <WeatherListItem
+//           key = {day.dt}
+//           day = {day}
+//           onDayClicked={onDayClicked}
+//           index={index}
+//           />
+//       )}
+//     </div>;
+//   }
+// }
 
 export default WeatherList;
